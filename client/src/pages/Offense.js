e761f36336f203d09f5bc3e4bd49133aa70e4f27
Jdentity <identityjang@gmail.com>
2022-03-09 21:20:25 +0900

    [fix] metaRecord percentage

--- client/src/pages/Offense.js
+++ client/src/pages/Offense.js
@@ -1,6 +1,7 @@
 import { Autocomplete, Box, TextField, ImageList, ImageListItem } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import HeroDataApi from '../api/HeroDataApi';
+import heroesById from '../components/HeroesById';
 import Assassin from '../images/classassassin.png';
 import Knight from '../images/classknight.png';
 import Mage from '../images/classmage.png';
@@ -13,6 +14,7 @@ import Fire from '../images/elementfire.png';
 import Ice from '../images/elementice.png';
 import Light from '../images/elementlight.png';
 import questionCircle from "../images/question_circle.png";
+import OffenseMetaApi from '../api/OffenseMetaApi';
 
 const attributeImgs = {
   fire: Fire,
@@ -30,6 +32,13 @@ const roleImgs = {
   ranger: Ranger,
   warrior: Warrior
 };
+
+const objectFlip = (obj) => {
+  return Object.keys(obj).reduce((ret, key) => {
+    ret[obj[key]] = key;
+    return ret;
+  }, {});
+}
 
 const HeroSelector = ({ data, text, changeHandler }) => {
   data = Object.values(data);
@@ -50,7 +59,7 @@ const HeroSelector = ({ data, text, changeHandler }) => {
             loading="lazy"
             width="50"
             src={option.assets.icon}
-            alt={option.assets.name}
+            alt={option.name}
           />
           <img
             loading="lazy"
@@ -87,22 +96,50 @@ function Offense() {
   const [third, setThird] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [metaData, setMetaData] = useState([]);
+  const IdByHeroes = objectFlip(heroesById);
 
   const handleFirstSelector = (event) => {
-    setFirst(event.target.innerText);
+    // console.log(event);
+    // console.log('value : ' + event.target.value);
+    // console.log('innerText : ' + event.target.innerText);
+    setFirst(heroes[event.target.innerText]);
+    // console.log(first);
   };
 
   const handleSecondSelector = (event) => {
-    setSecond(event.target.innerText);
+    setSecond(heroes[event.target.innerText]);
   };
 
   const handleThirdSelector = (event) => {
-    setThird(event.target.innerText);
+    setThird(heroes[event.target.innerText]);
   };
 
   const preventDragHandler = (event) => {
     event.preventDefault();
   }
+
+  const getOffenseMetaData = useCallback(async () => {
+    if(first === "" || second === "" || third === ""){
+      return
+    }
+    const selected = [
+      IdByHeroes[first.name],
+      IdByHeroes[second.name],
+      IdByHeroes[third.name]
+    ].sort();
+    console.log(selected);
+    // setLoading(true);
+    await OffenseMetaApi(selected)
+      .then(res => {
+        setMetaData(res.data);
+        console.log(res.data);
+      });
+      // .catch(error => {
+      //   setError(error);
+      //   setLoading(false);
+      // });
+  }, [first, second, third, IdByHeroes]);
 
   const getHeroData = async () => {
     setLoading(true);
@@ -121,10 +158,21 @@ function Offense() {
     getHeroData();
   }, []);
 
+  useEffect(() => {
+    getOffenseMetaData();
+  }, [getOffenseMetaData]);
+
+  console.log(heroes);
+  console.log(metaData);
+
   if(loading) return <div>Loading...</div>;
   if(error) return <div>Error!</div>;
   return (
-    <Box>
+    <Box
+      sx={{
+        width: 465,
+      }}
+    >
       <Box>
         <ImageList
           display='fix'
@@ -139,7 +187,7 @@ function Offense() {
           >
             {(first === "")
               ? <img src={questionCircle} alt="icon" />
-              : <img src={heroes[first].assets.icon} alt="icon" />
+              : <img src={first.assets.icon} alt="icon" />
             }
           </ImageListItem>
           <ImageListItem 
@@ -147,7 +195,7 @@ function Offense() {
           >
           {(second === "")
               ? <img src={questionCircle} alt="icon" />
-              : <img src={heroes[second].assets.icon} alt="icon" />
+              : <img src={second.assets.icon} alt="icon" />
             }
           </ImageListItem>
           <ImageListItem 
@@ -155,7 +203,7 @@ function Offense() {
           >
           {(third === "")
               ? <img src={questionCircle} alt="icon" />
-              : <img src={heroes[third].assets.icon} alt="icon" />
+              : <img src={third.assets.icon} alt="icon" />
             }
           </ImageListItem>
         </ImageList>
